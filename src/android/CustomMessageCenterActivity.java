0371aedf39e52d18bda0fc737c5a65113daa51a2
Nicolás Loaiza <=>
2018-07-23 20:04:59 -0500

    Set icon color to white, status bar color to blue

--- src/android/CustomMessageCenterActivity.java
+++ src/android/CustomMessageCenterActivity.java
@@ -10,6 +10,12 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.Color;
 import android.view.View;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.content.Context;
+import android.view.WindowManager;
+import android.view.Window;
 
 import com.urbanairship.messagecenter.MessageCenterActivity;
 import com.urbanairship.messagecenter.MessageCenterFragment;
@@ -42,11 +48,31 @@ public class CustomMessageCenterActivity extends MessageCenterActivity {
             title =(String)bundle.get("TITLE");
             if(title != null && !title.isEmpty()){
                 getActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>" + title + "</font>"));
-                ColorDrawable cd = new ColorDrawable(0xFF041A32);
+                ColorDrawable cd = new ColorDrawable(0xFF13273B);
                 getActionBar().setBackgroundDrawable(cd);
+                changeBackArrowColor(getApplicationContext(), 243);
             }
-        }        
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.parseColor("#FF13273B"));
+        }      
     }
+
+    private Drawable changeBackArrowColor(Context context, int color) {
+        String resName;
+        int res;
+
+        resName = Build.VERSION.SDK_INT >= 23 ? "abc_ic_ab_back_material" : "abc_ic_ab_back_mtrl_am_alpha";
+        res = context.getResources().getIdentifier(resName, "drawable", context.getPackageName());
+
+        final Drawable upArrow = context.getResources().getDrawable(res);
+        upArrow.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        getActionBar().setHomeAsUpIndicator(upArrow);
+        return upArrow;
+    }    
 
     @Override
     protected void onNewIntent(Intent intent) {
